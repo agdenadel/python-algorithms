6dda3dec237865cd0ccdcd431c5bc7ca418cedbd
agdenadel <agdenadel@gmail.com>
2017-10-13 23:35:22 -0400

    Add merge sort

---
+++ algorithms/sorting/merge_sort.py
@@ -0,0 +1,21 @@
+def merge_sort(array):
+    def merge(arr1, arr2):
+        i = j = 0
+        merged = []
+        while i < len(arr1) or j < len(arr2):
+            if i == len(arr1) or j == len(arr2):
+                merged.extend(arr2[j:])
+                merged.extend(arr1[i:])
+                break
+            elif arr1[i] < arr2[j]:
+                merged.append(arr1[i])
+                i += 1
+            elif arr1[i] > arr2[j]:
+                merged.append(arr2[j])
+                j += 1
+        return merged
+
+    if len(array) == 1:
+        return array
+
+    return merge(merge_sort(array[0:len(array) // 2]), merge_sort(array[len(array) // 2:]))
---
+++ algorithms/sorting/sorting_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from merge_sort import merge_sort
+
+
+class SortingTests(unittest.TestCase):
+    def test_merge_sort(self):
+        self.sort_tester(merge_sort)
+
+    def sort_tester(self, sort_function):
+        array = [2, 3, 1, 5, 6, 4, 0]
+
+        self.assertEqual(sort_function(array), [0, 1, 2, 3, 4, 5, 6])
+
+if __name__ == '__main__':
+    unittest.main()
